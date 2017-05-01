88e52a9fa1b175a35411729270d83570662e869c
chinatip <chinatip.v@ku.th>
2017-05-01 23:00:57 +0700

    Add 3-express-morgan

---
+++ 3-express-morgan/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+var morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+app.use('/', function(req, res){
+    res.send('Our first Express program');
+});
+
+app.listen(port);
+console.log('Server running on port: ' + port);
