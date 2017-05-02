2811da0e783b03902dc173f397a903db74c18fd7
chinatip <chinatip.v@ku.th>
2017-05-02 10:55:42 +0700

    Add 5-mongoose

--- 3-express-morgan/server.js
+++ 3-express-morgan/server.js
@@ -2,9 +2,13 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
+var mongoose = require('mongoose');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var morgan = require('morgan');
+
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
 
 app.use(morgan('dev'));
 app.use(cookieParser());
@@ -14,12 +18,14 @@ app.use(session({
         resave: true
 }));
 
-app.use('/', function(req, res){
-    res.send('Our first Express program');
-    console.log('Cookies: ', req.cookies);
-    console.log('--------------');
-    console.log(req.session);
-});
+// app.use('/', function(req, res){
+//     res.send('Our first Express program');
+//     console.log('Cookies: ', req.cookies);
+//     console.log('--------------');
+//     console.log(req.session);
+// });
+
+require("./app/routes.js")(app);
 
 app.listen(port);
 console.log('Server running on port: ' + port);
