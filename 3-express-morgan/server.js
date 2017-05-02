b4af0fcc11a6d99bd0e488c359edcd6e2794b53e
chinatip <chinatip.v@ku.th>
2017-05-02 12:14:04 +0700

    Add 6-ejs-body-parser

--- 3-express-morgan/server.js
+++ 3-express-morgan/server.js
@@ -6,17 +6,21 @@ var mongoose = require('mongoose');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 var configDB = require('./config/database.js');
 mongoose.connect(configDB.url);
 
 app.use(morgan('dev'));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(session({
         secret: 'anystringoftext',
         saveUninitialized: true,
         resave: true
 }));
+
+app.set('view engine', 'ejs');
 
 // app.use('/', function(req, res){
 //     res.send('Our first Express program');
