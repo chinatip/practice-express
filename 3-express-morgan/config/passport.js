8d0b8994fbbadc04ca8614819655b4185d7fb4db
chinatip <chinatip.v@ku.th>
2017-05-12 12:33:54 +0700

    Add 12-linking-social-accounts

--- 3-express-morgan/config/passport.js
+++ 3-express-morgan/config/passport.js
@@ -29,7 +29,8 @@ module.exports = function(passport) {
                     return done(err);
                 if(user) {
                     return done(null, false, req.flash('signupMessage', 'That email already taken'));
-                } else {
+                } 
+                if(!req.user) {
                     var newUser = new User();
                     newUser.local.username = email;
                     newUser.local.password = newUser.generateHash(password);
@@ -39,6 +40,17 @@ module.exports = function(passport) {
                             throw err;
                         return done(null, newUser);
                     });
+                }
+                else {
+                    var user = req.user;
+					user.local.username = email;
+					user.local.password = user.generateHash(password);
+
+					user.save(function(err){
+						if(err)
+							throw err;
+						return done(null, user);
+					});
                 }
                 
             });
@@ -67,43 +79,62 @@ module.exports = function(passport) {
     passport.use(new FacebookStrategy({
 	    clientID: configAuth.facebookAuth.clientID,
 	    clientSecret: configAuth.facebookAuth.clientSecret,
-	    callbackURL: configAuth.facebookAuth.callbackURL
-        // ,
-        // passReqToCallback : true,
-        // profileFields: ['id', 'emails', 'name']
+	    callbackURL: configAuth.facebookAuth.callbackURL,
+        passReqToCallback : true
 	  },
-	  function(accessToken, refreshToken, profile, done) {
+	  function(req, accessToken, refreshToken, profile, done) {
 	    	process.nextTick(function(){
-	    		User.findOne({'facebook.id': profile.id}, function(err, user) {
-	    			if(err)
-	    				return done(err);
-	    			if(user)
-	    				return done(null, user);
-	    			else {
-	    				var newUser = new User();
-	    				newUser.facebook.id = profile.id;
-	    				newUser.facebook.token = accessToken;
 
-	    				
-
-	    				
-	    				
-                        axios.get('https://graph.facebook.com/me?fields=email,name&access_token=' + accessToken)
-                            .then(function (response) {
-                                newUser.facebook.email = response.data.email;
-                                newUser.facebook.name = response.data.name;
-                                console.log(response.data)
-                                newUser.save(function(err){
-                                    if(err)
-                                        throw err;
-                                    return done(null, newUser);
+                //user is not logged it yet
+                if(!req.user) {
+	    		    User.findOne({'facebook.id': profile.id}, function(err, user) {
+                        if(err)
+                            return done(err);
+                        if(user)
+                            return done(null, user);
+                        else {
+                            var newUser = new User();
+                            newUser.facebook.id = profile.id;
+                            newUser.facebook.token = accessToken;
+                            
+                            axios.get('https://graph.facebook.com/me?fields=email,name&access_token=' + accessToken)
+                                .then(function (response) {
+                                    newUser.facebook.email = response.data.email;
+                                    newUser.facebook.name = response.data.name;
+                                    console.log(response.data)
+                                    newUser.save(function(err){
+                                        if(err)
+                                            throw err;
+                                        return done(null, newUser);
+                                    });
+                                })
+                                .catch(function (error) {
+                                    console.log(error);
                                 });
-                            })
-                            .catch(function (error) {
-                                console.log(error);
+                            }
+                    });
+                }
+                //user is logged in already, and needs to be merged
+
+                else {
+                    var user = req.user;
+	    			user.facebook.id = profile.id;
+	    			user.facebook.token = accessToken;
+	    			axios.get('https://graph.facebook.com/me?fields=email,name&access_token=' + accessToken)
+                        .then(function (response) {
+                            user.facebook.email = response.data.email;
+                            user.facebook.name = response.data.name;
+                            console.log(response.data)
+                            user.save(function(err){
+                                if(err)
+                                    throw err;
+                                return done(null, user);
                             });
-	    			    }
-	    		});
+                        })
+                        .catch(function (error) {
+                            console.log(error);new User()
+                        });
+                }
 	    	});
 	    }
 
@@ -112,30 +143,48 @@ module.exports = function(passport) {
     passport.use(new GoogleStrategy({
 	    clientID: configAuth.googleAuth.clientID,
 	    clientSecret: configAuth.googleAuth.clientSecret,
-	    callbackURL: configAuth.googleAuth.callbackURL
+	    callbackURL: configAuth.googleAuth.callbackURL,
+	    passReqToCallback: true
 	  },
-	  function(accessToken, refreshToken, profile, done) {
+	  function(req, accessToken, refreshToken, profile, done) {
 	    	process.nextTick(function(){
-	    		User.findOne({'google.id': profile.id}, function(err, user){
-	    			if(err)
-	    				return done(err);
-	    			if(user)
-	    				return done(null, user);
-	    			else {
-	    				var newUser = new User();
-	    				newUser.google.id = profile.id;
-	    				newUser.google.token = accessToken;
-	    				newUser.google.name = profile.displayName;
-	    				newUser.google.email = profile.emails[0].value;
+                //user is not logged it yet
+                if(!req.user) {
+                    User.findOne({'google.id': profile.id}, function(err, user){
+                        if(err)
+                            return done(err);
+                        if(user)
+                            return done(null, user);
+                        else {
+                            var newUser = new User();
+                            newUser.google.id = profile.id;
+                            newUser.google.token = accessToken;
+                            newUser.google.name = profile.displayName;
+                            newUser.google.email = profile.emails[0].value;
 
-	    				newUser.save(function(err){
-	    					if(err)
-	    						throw err;
-	    					return done(null, newUser);
-	    				})
-	    				console.log(profile);
-	    			}
-	    		});
+                            newUser.save(function(err){
+                                if(err)
+                                    throw err;
+                                return done(null, newUser);
+                            });
+                            console.log(profile);
+                        }
+                    });  
+                }
+                //user is logged in already, and needs to be merged
+                else {
+                    var user = req.user;
+                            user.google.id = profile.id;
+                            user.google.token = accessToken;
+                            user.google.name = profile.displayName;
+                            user.google.email = profile.emails[0].value;
+
+                            user.save(function(err){
+                                if(err)
+                                    throw err;
+                                return done(null, user);
+                            });
+                }
 	    	});
 	    }
 
